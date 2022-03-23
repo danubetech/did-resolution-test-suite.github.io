df1a9022ca8ae782baec2a926463c09d388dcdec
zaidarivai <zaida.rivai@danubetech.com>
2022-03-23 11:26:30 +0100

    set back specs

---
+++ cypress/integration/uniresolver_fixtures_spec.js
@@ -0,0 +1,213 @@
+const endpoint = Cypress.env("endpoint");
+
+if (Cypress.env("TEST_200") == true) {
+  describe(
+    "Test Scenario 1: DID Resolution Result fixtures: " + endpoint,
+    () => {
+      it("A correct DID can be resolved", () => {
+        cy.fixture("../fixtures/example_dids.json")
+          .its("normalDids")
+          .then((list) => {
+            Object.keys(list).forEach((key) => {
+              const normalDid = list[key];
+              cy.request({
+                method: "GET",
+                url: endpoint + normalDid,
+              }).as("request");
+
+              cy.get("@request").then((response) => {
+                expect(response.status).to.eq(200);
+              });
+
+              cy.get("@request").then((response) => {
+                expect(response.headers["content-type"]).to.contain(
+                  'application/ld+json;profile="https://w3id.org/did-resolution'
+                );
+              });
+
+              cy.get("@request").then((response) => {
+                expect(response.body).to.have.property("didDocument");
+              });
+
+              cy.get("@request").then((response) => {
+                expect(response.body).to.have.property("didResolutionMetadata");
+              });
+
+              cy.get("@request").then((response) => {
+                expect(response.body).to.have.property("didDocumentMetadata");
+              });
+            });
+          });
+      });
+    }
+  );
+}
+
+if (Cypress.env("TEST_200JLD") == true) {
+  describe("Test Scenario 2: JSON-LD DID document", () => {
+    it("A correct DID can be resolved with header input", () => {
+      cy.fixture("../fixtures/example_dids.json")
+        .its("normalDids")
+        .then((list) => {
+          Object.keys(list).forEach((key) => {
+            const normalDid = list[key];
+            cy.request({
+              method: "GET",
+              url: endpoint + normalDid,
+              headers: { Accept: "application/did+ld+json" },
+            }).as("headers");
+            cy.get("@headers").then((response) => {
+              expect(response.status).to.eq(200);
+              expect(response.headers["content-type"]).to.contain(
+                "application/did+ld+json"
+              );
+              expect(response.body).not.to.have.property("didDocument");
+              expect(response.body).to.have.property("@context");
+            });
+          });
+        });
+    });
+  });
+}
+
+if (Cypress.env("TEST_406") == true) {
+  describe("Test Scenario 3: Representation not supported", () => {
+    it("Shows an error when a representation is prompted", () => {
+      cy.fixture("../fixtures/example_dids.json")
+        .its("normalDids")
+        .then((list) => {
+          Object.keys(list).forEach((key) => {
+            const normalDid = list[key];
+
+            cy.request({
+              method: "GET",
+              url: endpoint + normalDid,
+              headers: { Accept: "image/png" },
+              failOnStatusCode: false,
+            }).then((response) => {
+              expect(response.status).to.eq(406);
+            });
+          });
+        });
+    });
+  });
+}
+
+if (Cypress.env("TEST_410") == true) {
+  describe("Test Scenario 4: Deactivated", () => {
+    it("Returns an HTTP code of 410 for deactivated DIDs", () => {
+      cy.fixture("../fixtures/example_dids.json")
+        .its("deacDids")
+        .then((list) => {
+          Object.keys(list).forEach((key) => {
+            const deacDid = list[key];
+
+            cy.request({
+              method: "GET",
+              url: endpoint + deacDid,
+              failOnStatusCode: false,
+            }).then((response) => {
+              expect(response.status).to.eq(410);
+              expect(response.headers["content-type"]).to.contain(
+                'application/ld+json;profile="https://w3id.org/did-resolution"'
+              );
+              //FAILS
+              expect(response.body.didDocumentMetadata.deactivated).to.eq(true);
+            });
+          });
+        });
+    });
+  });
+}
+
+if (Cypress.env("TEST_404") == true) {
+  describe("Test Scenario 5: Not found", () => {
+    it("Returns an HTTP code of 404 for non-existent DIDs", () => {
+      cy.fixture("../fixtures/example_dids.json")
+        .its("nonExisDids")
+        .then((list) => {
+          Object.keys(list).forEach((key) => {
+            const nonExDid = list[key];
+
+            cy.request({
+              method: "GET",
+              url: endpoint + nonExDid,
+              failOnStatusCode: false,
+            }).then((response) => {
+              expect(response.status).to.eq(404);
+              expect(response.headers["content-type"]).to.contain(
+                'application/ld+json;profile="https://w3id.org/did-resolution"'
+              );
+              expect(response.body.didResolutionMetadata.error).to.eq(
+                "notFound"
+              );
+            });
+          });
+        });
+    });
+  });
+}
+
+if (Cypress.env("TEST_400") == true) {
+  describe("Test Scenario 6: Invalid DID", () => {
+    it("Returns a HTTP code of 400 for an invalid DID", () => {
+      cy.fixture("../fixtures/example_dids.json")
+        .its("invalidDids")
+        .then((list) => {
+          Object.keys(list).forEach((key) => {
+            const invalidDid = list[key];
+
+            cy.request({
+              method: "GET",
+              url: endpoint + invalidDid,
+              failOnStatusCode: false,
+            }).then((response) => {
+              expect(response.status).to.eq(400);
+              expect(response.headers["content-type"]).to.contain(
+                'application/ld+json;profile="https://w3id.org/did-resolution"'
+              );
+              expect(response.body.didResolutionMetadata.error).to.eq(
+                "invalidDid"
+              );
+            });
+          });
+        });
+    });
+  });
+}
+
+if (Cypress.env("TEST_200F") == true) {
+  describe("Test Scenario 7: DID URLs with fragments", () => {
+    it("Can resolve a DID with a fragment", () => {
+      cy.fixture("../fixtures/example_dids.json")
+        .its("fragmentDids")
+        .then((list) => {
+          Object.keys(list).forEach((key) => {
+            const fragmentDid = list[key];
+
+            cy.request({
+              method: "GET",
+              url: endpoint + fragmentDid,
+              failOnStatusCode: false,
+              headers: { Accept: "application/did+ld+json" },
+            }).as("request");
+
+            cy.get("@request").then((response) => {
+              console.log(response);
+              expect(response.status).to.eq(200);
+              expect(response.headers["content-type"]).to.contain(
+                "application/did+ld+json"
+              );
+              expect(response.body).not.to.have.property("didDocument");
+              expect(response.body).to.have.property("@context");
+              expect(response.body).to.have.property("type");
+              expect(response.body).to.have.property("id");
+              expect(response.body["id"]).to.contain(
+                decodeURIComponent(fragmentDid.split("#")[0])
+              );
+            });
+          });
+        });
+    });
+  });
+}
