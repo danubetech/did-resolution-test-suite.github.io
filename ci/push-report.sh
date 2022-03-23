13896a1a705eb6e8139e0b45deacdf636fd896fe
BernhardFuchs <bernhard.fuchs@danubetech.com>
2022-03-23 13:14:33 +0100

    chore: better name for generated folder

--- ci/push-report.sh
+++ ci/push-report.sh
@@ -5,18 +5,18 @@ ls -l /home/runner/work/did-resolution-test-suite/did-resolution-test-suite/repo
 
 echo "Save tests to folder"
 DATE_WITH_TIME=$(TZ=UTC date "+%Y-%m-%d_%H:%M:%S")
-REPORTS_FOLDER="/home/runner/work/did-resolution-test-suite/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME-api-tests"
+REPORT_FOLDER="/home/runner/work/did-resolution-test-suite/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME"
 
-mkdir "$REPORTS_FOLDER"
-cp -r /home/runner/work/did-resolution-test-suite/did-resolution-test-suite/reports/mochareports/ "$REPORTS_FOLDER"
+mkdir "$REPORT_FOLDER"
+cp -r /home/runner/work/did-resolution-test-suite/did-resolution-test-suite/reports/ "$REPORT_FOLDER"
 echo "gh-pages folder"
 ls -l /home/runner/work/did-resolution-test-suite/did-resolution-test-suite/gh-pages/
 echo "reports folder"
-ls -l "$REPORTS_FOLDER"
+ls -l "$REPORT_FOLDER"
 
 #echo "Push result file to repo"
 #git config --global user.email "admin@danubetech.com"
 #git config --global user.name "Did Resolution tests"
-#git add "$REPORTS_FOLDER"
+#git add "$REPORT_FOLDER"
 #git commit -m "$DATE_WITH_TIME Did Resolution test report"
 #git push origin main:gh-pages
