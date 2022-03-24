40755c6d9c9199ee603ba9297b8dd7b6bb75be53
BernhardFuchs <bernhard.fuchs@danubetech.com>
2022-03-24 15:29:06 +0100

    fix: switch to gh-pages branch before copying files

--- ci/push-report.sh
+++ ci/push-report.sh
@@ -3,6 +3,11 @@
 echo "Created following report:"
 ls -l "$PWD/reports/mochareports/"
 
+echo "Switch to gh-pages branch"
+git fetch
+git switch gh-pages
+git status
+
 echo "Save tests to folder"
 DATE_WITH_TIME=$(TZ=UTC date "+%Y-%m-%d_%H:%M:%S")
 REPORT_FOLDER="$PWD/gh-pages/$DATE_WITH_TIME"
@@ -23,4 +28,4 @@ git config --global user.name "Did Resolution tests"
 git add "$REPORT_FOLDER"
 git add index.md
 git commit -m "$DATE_WITH_TIME Did Resolution test report"
-git push origin main:gh-pages
+git push origin gh-pages:gh-pages
