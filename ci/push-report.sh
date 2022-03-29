10692c1abf49705459bf71583baae6cc868349af
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2022-03-29 14:08:00 +0200

    Fix link substituion (#7)
    * fix: sed command with different delimiters and non-empty file
    
    * chore: add ls log
    
    * chore: add newlines
    
    * fix: add pwd folder to git add
    
    * fix: add new line
    
    * chore: remove extra newline
    
    * fix: add newline in separate command
    
    * fix: unterminated s command
    
    * fix: use separate newline command after adding link
    
    * fix: use two newlines because of markdown formatting
    
    * fix: use two empty spaces on end of line
    
    * feat: customize title and description
    
    * chore: cleanup debug logs

--- ci/push-report.sh
+++ ci/push-report.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -x
-
 echo "Created following report:"
 ls -l "$PWD/reports/mochareports/"
 
@@ -13,22 +11,16 @@ git status
 echo "Save tests to folder"
 DATE_WITH_TIME=$(TZ=UTC date "+%Y-%m-%d_%H:%M:%S")
 REPORT_FOLDER="$PWD/gh-pages/$DATE_WITH_TIME"
-
 mkdir "$REPORT_FOLDER"
 cp -r "$PWD/reports/." "$REPORT_FOLDER"
-echo "gh-pages folder"
-ls -l "$PWD/gh-pages/"
-echo "reports folder"
-ls -l "$REPORT_FOLDER"
 
 echo "Add Link to main page"
-sed -i "1s|^|[$DATE_WITH_TIME](https://danubetech.github.io/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME/mochareports/reports.html)\n|" "$PWD/index.md"
-cat "$PWD/index.md"
+sed -i "1s|^|[$DATE_WITH_TIME](https://danubetech.github.io/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME/mochareports/reports.html)  \n|" "$PWD/index.md"
 
 echo "Push result file to repo"
 git config --global user.email "admin@danubetech.com"
 git config --global user.name "Did Resolution tests"
 git add "$REPORT_FOLDER"
-git add index.md
+git add "$PWD/index.md"
 git commit -m "$DATE_WITH_TIME Did Resolution test report"
 git push origin gh-pages:gh-pages
