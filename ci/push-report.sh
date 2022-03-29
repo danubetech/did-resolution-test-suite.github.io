57d2914a4cd615671d1b24e61899fb561bdefc82
BernhardFuchs <bernhard.fuchs@danubetech.com>
2022-03-29 14:06:20 +0200

    chore: cleanup debug logs

--- ci/push-report.sh
+++ ci/push-report.sh
@@ -11,21 +11,11 @@ git status
 echo "Save tests to folder"
 DATE_WITH_TIME=$(TZ=UTC date "+%Y-%m-%d_%H:%M:%S")
 REPORT_FOLDER="$PWD/gh-pages/$DATE_WITH_TIME"
-
 mkdir "$REPORT_FOLDER"
 cp -r "$PWD/reports/." "$REPORT_FOLDER"
 
-set -x
-printf "\n"
 echo "Add Link to main page"
-printf "\n"
-ls -l "$PWD"
-printf "\n"
 sed -i "1s|^|[$DATE_WITH_TIME](https://danubetech.github.io/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME/mochareports/reports.html)  \n|" "$PWD/index.md"
-printf "\n"
-echo "cat index.md"
-cat "$PWD/index.md"
-printf "\n"
 
 echo "Push result file to repo"
 git config --global user.email "admin@danubetech.com"
