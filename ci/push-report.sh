a667816c30ece54c0b0c30d3cea53c6acb01d693
BernhardFuchs <bernhard.fuchs@danubetech.com>
2022-04-04 18:37:02 +0200

    fix: use env variable instead of deprecated set-env

--- ci/push-report.sh
+++ ci/push-report.sh
@@ -15,8 +15,8 @@ mkdir "$REPORT_FOLDER"
 cp -r "$PWD/reports/." "$REPORT_FOLDER"
 
 echo "Add Link to main page"
-export LINK="https://danubetech.github.io/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME/mochareports/reports.html"
-sed -i "1s|^|[$DATE_WITH_TIME]($LINK)  \n|" "$PWD/index.md"
+export REPORT_LINK="https://danubetech.github.io/did-resolution-test-suite/gh-pages/$DATE_WITH_TIME/mochareports/reports.html"
+sed -i "1s|^|[$DATE_WITH_TIME]($REPORT_LINK)  \n|" "$PWD/index.md"
 
 echo "Push result file to repo"
 git config --global user.email "admin@danubetech.com"
@@ -27,5 +27,5 @@ git commit -m "$DATE_WITH_TIME Did Resolution test report"
 git push origin gh-pages:gh-pages
 
 echo "Set link as env variable"
-echo "::set-env name=link::$LINK"
-echo "$LINK"
+echo "report_link=$REPORT_LINK" >> "$GITHUB_ENV"
+echo "$REPORT_LINK"
