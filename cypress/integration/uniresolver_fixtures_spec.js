c14278eb3b888113d22ac65012418fbd1c2bf956
zaida-rivai <36229830+zaida-rivai@users.noreply.github.com>
2022-05-05 13:51:06 +0200

    removed test 10 and 11 (#19)
    Co-authored-by: zaidarivai <zaida.rivai@danubetech.com>

--- cypress/integration/uniresolver_fixtures_spec.js
+++ cypress/integration/uniresolver_fixtures_spec.js
@@ -317,76 +317,6 @@ if (Cypress.env("TEST_200_TK") == true) {
   });
 }
 
-if (Cypress.env("TEST_200_VT") == true) {
-  describe("Test Scenario 10: DID URLs with versionTime parameter", () => {
-    it("MUST return HTTP response status 200", () => {
-      cy.fixture("../fixtures/example_dids.json")
-        .its("versionTimeDids")
-        .then((list) => {
-          Object.keys(list).forEach((key) => {
-            const versionTimeDid = list[key];
-
-            cy.request({
-              method: "GET",
-              url: endpoint + versionTimeDid,
-              headers: {
-                Accept: "application/did+ld+json",
-                Authorization: "Bearer b082c420-df67-4b06-899c-b7c51d75fba0",
-              },
-              failOnStatusCode: false,
-            }).as("request");
-
-            cy.get("@request").then((response) => {
-              expect(response.status).to.eq(200);
-              expect(response.headers["content-type"]).contains(
-                "application/did+ld+json"
-              );
-              expect(response).to.be.a("object");
-              expect(response.body)
-                .has.property("id")
-                .contains("did:sov:DjxRxnL4gXsncbH8jM8ySM");
-            });
-          });
-        });
-    });
-  });
-}
-
-if (Cypress.env("TEST_200_VI") == true) {
-  describe("Test Scenario 11: DID URLs with versionId parameter", () => {
-    it("MUST return HTTP response status 200", () => {
-      cy.fixture("../fixtures/example_dids.json")
-        .its("versionIdDids")
-        .then((list) => {
-          Object.keys(list).forEach((key) => {
-            const versionIdDid = list[key];
-
-            cy.request({
-              method: "GET",
-              url: endpoint + versionIdDid,
-              headers: {
-                Accept: "application/did+ld+json",
-                Authorization: "Bearer b082c420-df67-4b06-899c-b7c51d75fba0",
-              },
-              failOnStatusCode: false,
-            }).as("request");
-
-            cy.get("@request").then((response) => {
-              expect(response.status).to.eq(200);
-              expect(response).to.be.a("object");
-              expect(response.headers["content-type"]).contains(
-                "application/did+ld+json"
-              );
-              expect(response.body)
-                .has.property("id")
-                .contains(decodeURIComponent(versionIdDid.split("?")[0]));
-            });
-          });
-        });
-    });
-  });
-}
-
 if (Cypress.env("TEST_200_DURL") == true) {
   describe("Test Scenario 12: Resolve a DID / dereference a DID URL", () => {
     it("MUST return HTTP response status 200", () => {
@@ -419,7 +349,6 @@ if (Cypress.env("TEST_200_DURL") == true) {
   });
 }
 
-//todo: HEADER IS NOT ACCEPTED WHY?
 if (Cypress.env("TEST_200_DRURL") == true) {
   describe("Test Scenario 12B: Resolve a DID / dereference a DID URL", () => {
     it("MUST return HTTP response status 200", () => {
